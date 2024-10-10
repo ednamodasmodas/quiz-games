358177893990bef7875b79329f8cc9ac85408ea4
ednamodasmodas <00001107210501sp@al.educacao.sp.gov.br>
2024-10-10 10:23:54 -0300

    quiz para jogadores virtuais

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'games',
+    'qual o jogo mais vendido no mundo?',
+    'minecraft possui mais de 300 milhões de cópias vendidas'
+)
+
+criaCartao(
+    'games',
+    'quem são os irmãos encanadores mais conhecidos dos games?',
+    'Mario e Luigi'
+)
+
+criaCartao(
+    'games',
+    'qual empresa criou a franquia God Of War?',
+    'Santa monica Studios da Sony'
+)
+
+criaCartao(
+    'games',
+    'Cj é um personagem famoso, protagonista do jogo...',
+    'Grand Theft Auto: San Andreas'
+)
